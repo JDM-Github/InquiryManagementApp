3ad52bf0be96ff93db7a0af643fbfab0b619e072
JDM-Github <jdmaster888@gmail.com>
2024-12-12 12:37:05 +0800

    Changes to be committed: 	modified:   Views/Shared/_Layout.cshtml 	modified:   obj/Debug/net9.0/InquiryManagementApp.AssemblyInfo.cs 	modified:   obj/Debug/net9.0/InquiryManagementApp.AssemblyInfoInputs.cache

--- obj/Debug/net9.0/InquiryManagementApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/InquiryManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InquiryManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b66494efbcd14b8a8dfceecde4c389743ee80cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("InquiryManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InquiryManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
